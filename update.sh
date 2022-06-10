5a0d15a861b83315392f8af788894a1475c329e4
jaek <jakethekoenig@gmail.com>
2022-06-10 04:40:29 -0700

    Removed janky update script

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git add -A
-git commit --amend --no-edit
-rm -rf ~/.vim/bundle/VimOutline/
-vi +PluginInstall +qall
-vi -O autoload/vimoutline.vim ftplugin/outline_settings.vim ftplugin/outline_mappings.vim
